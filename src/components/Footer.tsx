94485d8033fe55b5aa5a0babcdebbabff6316301
damzymichael <105460672+damzymichael@users.noreply.github.com>
2025-06-05 00:25:53 +0100

    fix for build error from import statement

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from "react"
 import Link from "next/link"
-import Logo from "./Logo"
+import Logo from "./global/Logo"
 import { usePathname } from "next/navigation"
 import { excludedLinks } from "@/lib/constants"
 
